f716739cd1461768a4adacec3a4f79af2e153ec4
yyhome-tromb <107352731+yyhome-tromb@users.noreply.github.com>
2023-05-30 13:20:59 +0900

    add page toggle(not complete)

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 console.log("Loaded");
+
+let page;
+
+const page_top=document.getElementById("page_top");
+const page_quiz=document.getElementById("page_quiz");
+const top_start=document.getElementById("top_start");
+
+
+top_start.addEventListener("click",e=>{
+   //start();
+   page_top.style.display="none";
+   page_quiz.style.display="flex";
+});
